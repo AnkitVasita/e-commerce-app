0a016ed9a73b6370cf5e40a270f8587b40f3b9cd
AnkitVasita <vasitaankit97@gmail.com>
2021-05-21 23:31:43 +0530

    Build : frontend

---
+++ src/components/Header.js
@@ -0,0 +1,77 @@
+import Image from "next/image";
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+
+const Header = () => {
+  return (
+    <header>
+      {/* top nav */}
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+          <Image
+            src="http://links.papareact.com/f90"
+            width={150}
+            height={40}
+            objectFit="contain"
+            className="cursor-pointer"
+          />
+        </div>
+
+        {/* search */}
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+          <input
+            type="text"
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4 "
+          />
+          <SearchIcon className="h-12 p-4" />
+        </div>
+
+        {/* right */}
+
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap ">
+          <div className="link">
+            <p>hello Ankit Vasita</p>
+            <p className="font-extrabold sm:text-sm">Account & Lists</p>
+          </div>
+
+          <div className="link">
+            <p>Returns</p>
+            <p className="font-extrabold sm:text-sm">& orders</p>
+          </div>
+
+          <div className=" relative link flex items-center">
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black  font-bold ">
+              0
+            </span>
+            <ShoppingCartIcon className="h-10" />
+            <p className=" hidden md:inline font-extrabold sm:text-sm">
+              Basket
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* bottom nav */}
+      <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
+        <p className="link flex items-center">
+          <MenuIcon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Todays's Deal</p>
+        <p className="link hidden lg:inline-flex">Electronics</p>
+        <p className="link hidden lg:inline-flex">Food & Grocery</p>
+        <p className="link hidden lg:inline-flex">Prime</p>
+        <p className="link hidden lg:inline-flex">Buy Again</p>
+        <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+        <p className="link hidden lg:inline-flex">Health & Personal Care</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/index.js
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
+
+export default function Home({ products }) {
+  return (
+    <div className="bg-gray-100 ">
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+
+      <Header />
+      <main className="max-w-screen-2xl mx-auto">
+        {/* banner */}
+        <Banner />
+        {/* products */}
+        <ProductFeed products={products} />
+      </main>
+    </div>
+  );
+}
+
+export const getServerSideProps = async (context) => {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
