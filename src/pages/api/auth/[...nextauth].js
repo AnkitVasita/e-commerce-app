11184cf76a36eaa401849756a23b19b2a147cafc
AnkitVasita <vasitaankit97@gmail.com>
2022-03-28 00:50:19 +0530

    changes

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth";
 // import Providers from "next-auth/providers";
 import GoogleProvider from "next-auth/providers/google";
-import Auth0Provider from "next-auth/providers/auth0";
 
 export default NextAuth({
   // Configure one or more authentication providers
