6448bd11610c3bb1753e94a22829b31752aa3bbd
AnkitVasita <vasitaankit97@gmail.com>
2022-03-28 23:40:14 +0530

    added key

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -11,6 +11,7 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  secret: process.env.SECRET,
   callbacks: {
     async session({ session, token }) {
       session.user.username = session.user.name
